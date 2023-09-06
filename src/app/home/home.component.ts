befe6ccd044df4c58ff4f33a10308e0d44522fac
Ghadeer-Moh <139185332+Ghadeer-Moh@users.noreply.github.com>
2023-09-06 14:40:30 +0300

    HomeComponent

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+}
