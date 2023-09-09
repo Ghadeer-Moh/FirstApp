2ea6e6f10189915437fe99636985b301fa4ceb05
Ghadeer-Moh <139185332+Ghadeer-Moh@users.noreply.github.com>
2023-09-09 19:02:46 +0300

    nested

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule} from "@angular/forms";
+import { FormsModule , ReactiveFormsModule} from "@angular/forms";
 
 
 import { HeaderComponent } from './Layout/header/header.component';
@@ -30,7 +30,8 @@ import { BtnComponent } from './btn/btn.component';
     BrowserModule,
     // RouterOutlet
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [IndexComponent]
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,6 @@
 import { Component , OnInit} from '@angular/core';
 import {User} from "./User";
+import { FormGroup ,FormControl} from "@angular/forms";
 
 
 @Component({
@@ -9,22 +10,34 @@ import {User} from "./User";
 })
 
 export class MainComponent implements OnInit {
-  submitted = false;
-  countries=['eg','ksa','uae','qtr'];
-user = new User('Angular','Angoular@domain.com','male','eg');
+  myModel = new FormGroup({
+    firstname: new FormControl(''),
+     lastname: new FormControl(''),
+
+  });
+//   submitted = false;
+//   countries=['eg','ksa','uae','qtr'];
+// user = new User('Angular','Angoular@domain.com','male','eg');
 
   constructor() {
+    // this.firstname.setValue('Angular');
   }
-  edit(){
-    this.submitted=true;
-  }
-  update(){
-    this.submitted=false;
-  }
+  // edit(){
+  //   this.submitted=true;
+  // }
+  // update(){
+  //   this.submitted=false;
+  // }
   ngOnInit() {
   }
 
-
+  update(){
+    // this.firstname.setValue('Angular');
+    this.myModel.patchValue({
+      firstname: 'Angular',
+      lastname: 'Framework',
+    })
+  }
 
 
 }
